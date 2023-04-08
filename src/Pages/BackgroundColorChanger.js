c3d68137545e74c93339bf2b395471a46dfc3f90
Pooja Birajdar <37832058+poojabirajdar@users.noreply.github.com>
2023-04-08 21:15:43 +0530

    Update BackgroundColorChanger.js

--- src/Pages/BackgroundColorChanger.js
+++ src/Pages/BackgroundColorChanger.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 export const BackgroundColorChanger = () =>{
+    const { colorname } = useParams();
     return(
+        <main style={{ height: '100vh', backgroundColor: colorname }}>
+      Background Color Changer
+    </main>
        
     )
 }
